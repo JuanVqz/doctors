bdab48fea24253dbdeae8cc473972b722debfff7
iTox <juanthoniovg@gmail.com>
2018-12-09 09:47:37 -0600

    Add validates to Hospital

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,7 +1,8 @@
 class Hospital < ApplicationRecord
   cattr_accessor :current_id
 
-  validates :subdomain, uniqueness: true
+  validates :subdomain, :name, presence: true
+  validates :subdomain, :name, uniqueness: true
 
   def to_s
     name.titleize
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Hospital, type: :model do
+  it { should validate_presence_of :name }
+  it { should validate_uniqueness_of :name }
+  it { should validate_presence_of :subdomain }
   it { should validate_uniqueness_of :subdomain }
 end
