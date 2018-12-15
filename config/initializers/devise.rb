e67029200f9db046d6837cdc5b5a938d2aaa89d8
iTox <juanthoniovg@gmail.com>
2018-12-15 15:44:00 -0600

    Add DeviseCustomMailer
    Add style for Devise emails
    Create helper for use subdomain in buttons on mail

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,13 +3,16 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.mailer.class_eval do
+    helper :subdomain
+  end
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '6467eb54e70abcfa5bbec916c00a175220127469d583ed2499c21b0a39effd32cacb37aa594e1f9ca58994c9ab1025898aaaf82db1770986354619f98f45a3f3'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -18,10 +21,11 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'notificaciones@asistenciaclinica.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
+  config.mailer = 'DeviseCustomMailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
