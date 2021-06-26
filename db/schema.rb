9182993bd545df7188897ad2494b26000d459c56
Juan Vasquez <juan@logicalbricks.com>
2021-06-26 17:38:13 -0500

    DOC-28 Mejora a Hospitalizaciones (#29)
    * DOC-28 Mejora a Hospitalizaciones
    
    issue #28
    
    [DOC-28](https://github.com/JuanVqz/doctors/issues/28)
    
    * DOC-28 add new attributes into Hospitalization table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_184852) do
+ActiveRecord::Schema.define(version: 2021_06_26_220616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,6 +125,9 @@ ActiveRecord::Schema.define(version: 2021_05_30_184852) do
     t.bigint "patient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "input_diagnosis"
+    t.text "output_diagnosis"
+    t.text "recommendations"
     t.index ["doctor_id"], name: "index_hospitalizations_on_doctor_id"
     t.index ["patient_id"], name: "index_hospitalizations_on_patient_id"
   end
