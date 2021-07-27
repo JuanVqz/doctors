14cf117baa0552f8d5649928dd989a916711f239
Juan Vasquez <juan@logicalbricks.com>
2021-07-26 20:05:42 -0500

    DOC-49  update ruby and rails (#50)
    - [x] ruby to 2.7.3
    - [x] rails to 6.1.4
    
    closes #49

--- config/application.rb
+++ config/application.rb
@@ -11,17 +11,14 @@ module Doctors
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
     config.time_zone = "America/Mexico_City"
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = "es-MX"
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
