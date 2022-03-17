2354ba9cafb3ef8cff49ffb2e1c3c9b35b8cb843
Juan Vasquez <juan@logicalbricks.com>
2022-03-17 14:06:45 -0600

    Remove sidebar search bar

--- app/assets/javascripts/sidebar.js
+++
@@ -1,5 +0,0 @@
-$(document).on("turbolinks:load", function () {
-  $("#patient_id").on("select2:select", function (event) {
-    window.location.href = `/appoinments/new?patient_id=${event.target.value}`;
-  });
-});
