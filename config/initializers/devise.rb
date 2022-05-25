75a1188fff324aa2869ef27d4b03570b0df461e0
Juan Vasquez <juan@ombulabs.com>
2022-05-25 00:43:19 -0500

    [DOC-191] Remove devise_custom_mailer class
    Closes #191
    Related #101
    
    - [x] Remove custom devise/mailer views
    - [x] Remove mails per subdomain config
    - [x] Remove subdomain_helper
    
    Those lines didn't allow me fix the zeitweirk autoloading
    ```
    config.mailer.class_eval do
      helper :subdomain
    end
    ```
    
    If want to send emails we may need this back

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,9 +3,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.mailer.class_eval do
-    helper :subdomain
-  end
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -25,7 +22,6 @@ Devise.setup do |config|
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
-  config.mailer = "DeviseCustomMailer"
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
