3594dd17e44f2f62fc756101b31486046d8071c4
Juan Vásquez <juan@ombulabs.com>
2022-05-24 22:44:17 -0500

    [DOC-186] Fix Pundit deprecation warning (#189)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  include Pundit
+  include Pundit::Authorization
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   layout :layout_by_resource
 
