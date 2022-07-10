0246d2bac47514d019a740c90c8d639f2e218b71
Juan Vásquez <juan@ombulabs.com>
2022-07-09 23:26:01 -0500

    DR-195 Run rails app:update (#210)
    Closes https://github.com/JuanVqz/doctors/issues/195

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 module Doctors
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     config.time_zone = "America/Mexico_City"
     config.i18n.default_locale = "es-MX"
