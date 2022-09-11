96a2df148f5dc722e2ad0c16fe3392e234cfcc59
Juan Vasquez <juan@ombulabs.com>
2022-09-10 23:02:25 -0500

    Show skunk report

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,8 @@
-require "simplecov"
-SimpleCov.start "rails"
+if ENV["COVERAGE"] == "true"
+  require "simplecov"
+  SimpleCov.start "rails"
+  puts "Using SimpleCov v#{SimpleCov::VERSION}"
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
