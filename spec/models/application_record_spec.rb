6191f609345e5bb64dedf392db148b219981d799
Juan Vásquez <juan@ombulabs.com>
2022-11-20 20:26:06 -0600

    Improve test (#268)

--- spec/models/application_record_spec.rb
+++ spec/models/application_record_spec.rb
@@ -2,23 +2,27 @@ require "rails_helper"
 
 RSpec.describe ApplicationRecord do
   describe ".recent" do
-    context "order by created_at desc" do
+    context "with Hospitalization" do
       let(:hospitalization) { create(:hospitalization) }
       let(:hospitalization_old) { create(:hospitalization, created_at: 2.days.ago) }
-
-      let(:appoinment) { create(:appoinment) }
-      let(:appoinment_old) { create(:appoinment, created_at: 2.days.ago) }
-
-      let(:patient) { create(:patient) }
-      let(:patient_old) { create(:patient, created_at: 2.days.ago) }
 
       it "returns new hospitalization first" do
         expect(Hospitalization.recent).to eq [hospitalization, hospitalization_old]
       end
+    end
+
+    context "with Appoinment" do
+      let(:appoinment) { create(:appoinment) }
+      let(:appoinment_old) { create(:appoinment, created_at: 2.days.ago) }
 
       it "returns new medical_consultation first" do
         expect(Appoinment.recent).to eq [appoinment, appoinment_old]
       end
+    end
+
+    context "with Patient" do
+      let(:patient) { create(:patient) }
+      let(:patient_old) { create(:patient, created_at: 2.days.ago) }
 
       it "returns new patient first" do
         expect(Patient.recent).to eq [patient, patient_old]
