4920b02a8788f94e51121bfe11c10a23592cc12a
Juan Vasquez <juan@ombulabs.com>
2023-08-24 23:21:08 -0600

    Install tailwind and config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/helpers/**/*.rb',
+    './app/views/**/*.{html,erb,haml}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
