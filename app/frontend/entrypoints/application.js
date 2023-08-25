d42570d889aefdc55abdaafb8fd20a3629aa6dec
Juan Vasquez <juan@ombulabs.com>
2023-08-24 18:45:09 -0600

    Add ViteRails, trix editor, fontawesome
    Links:
      - https://vite-ruby.netlify.app/guide/plugins.html
      - https://github.com/ElMassimo/vite_ruby/tree/main/vite-plugin-rails

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -26,4 +26,4 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 
 // Example: Import a stylesheet in app/frontend/index.css
 // import '~/index.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'trix'
