7c93a1c75e251765909c5bbba43a5d569588d243
Juan Vasquez <juan@ombulabs.com>
2023-09-03 01:49:17 -0600

    Watch files that include tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     './app/helpers/**/*.rb',
     './app/views/**/*.{html,erb,haml}',
+    './config/initializers/simple_form_tailwindcss.rb',
+    './config/locales/simple_form.*.yml',
   ],
   theme: {
     extend: {},
