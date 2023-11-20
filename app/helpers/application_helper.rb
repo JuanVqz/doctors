5956a88cf80b20f67fad25c09d640d2e735d0807
Juan Vasquez <juan@ombulabs.com>
2023-11-20 01:15:23 -0600

    Add buttons icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,4 +34,8 @@ module ApplicationHelper
   def blood_groups_for_select
     ["ARH+", "ORH+", "BRH+", "ABRH+", "ARH-", "ORH-", "BRH-", "ABRH-"]
   end
+
+  def primary_button
+    "text-white text-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+  end
 end
