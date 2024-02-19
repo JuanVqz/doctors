a0529560bfea716b03013d594642b94927b04ef2
Juan Vasquez <juan@ombulabs.com>
2024-02-19 00:14:12 -0600

    Add ApplicationHelper tests

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 
 RSpec.describe ApplicationHelper do
-  skip
+  it "sexos_for_select" do
+    expect(helper.sexos_for_select).to eq(["Femenino", "Masculino"])
+  end
+
+  it "blood_groups_for_select" do
+    groups = ["ARH+", "ORH+", "BRH+", "ABRH+", "ARH-", "ORH-", "BRH-", "ABRH-"]
+    expect(helper.blood_groups_for_select).to eq groups
+  end
 end
