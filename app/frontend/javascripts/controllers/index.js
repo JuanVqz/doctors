866a97d284c4b414ae63a90d3c75e1808e4c63c4
Juan Vásquez <javasgon@gmail.com>
2024-02-21 22:42:01 -0600

    Calculate IMC (#441)

--- app/frontend/javascripts/controllers/index.js
+++ app/frontend/javascripts/controllers/index.js
@@ -12,3 +12,6 @@ application.register('focus-field', FocusFieldController)
 
 import SidebarController from './sidebar_controller'
 application.register('sidebar', SidebarController)
+
+import ImcController from './imc_controller'
+application.register('imc', ImcController)
