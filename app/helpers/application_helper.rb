18487c61caaef4a64229dd33685d77a2147d125e
Juan Vasquez <juan@ombulabs.com>
2024-02-23 01:25:06 -0600

    Update i18n views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,6 +41,10 @@ module ApplicationHelper
     "text-white text-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
   end
 
+  def secondary_button
+    "text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+  end
+
   def sidebar_classes action
     classes = "flex p-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700"
     classes += " bg-gray-200 dark:bg-gray-700" if controller_name == action
