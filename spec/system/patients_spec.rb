ae1f77df82c3a305e18e17d26057c36c29b4d53e
Juan Vasquez <juan@ombulabs.com>
2024-02-25 22:29:03 -0600

    Use capybara firefox driver

--- spec/system/patients_spec.rb
+++ spec/system/patients_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Patient's flow", type: :system do
   before do
-    driven_by(:selenium_chrome_headless)
+    driven_by(:headless_firefox)
   end
 
   feature "Doctor" do
