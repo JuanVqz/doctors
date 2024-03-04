81c3b82ab068d4fd604993f81c6eadff5141d3cf
Juan Vasquez <juan@ombulabs.com>
2024-03-04 08:44:36 -0600

    Fix fixture_paths deprecation warning
    Related #459

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -52,7 +52,7 @@ end
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = %w(Rails.root.join("spec/fixtures"))
+  config.fixture_paths = %w(Rails.root.join("spec/fixtures"))
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
