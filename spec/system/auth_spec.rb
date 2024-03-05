945874631d93d741438d46b3a2b8465079044afd
Juan Vásquez <javasgon@gmail.com>
2024-03-05 00:44:13 -0600

    Update Devise initializer (#483)
    rails generate devise:install
    
    add sign out test, to confirm it works because we installed Turbo and
    some stuff changed on Devise
    
    https://github.com/heartcombo/devise/wiki/How-To:-Upgrade-to-Devise-4.9.0-%5BHotwire-Turbo-integration%5D
    https://github.com/heartcombo/devise/blob/main/CHANGELOG.md#490---2023-02-17

---
+++ spec/system/auth_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "Auth flow", type: :system do
+  before do
+    driven_by(:headless_firefox)
+  end
+
+  feature "Auth" do
+    scenario "signs out" do
+      create_hospital_plan_medium
+      sign_in_admin_doctor @hospital
+
+      expect(page).to have_content @admin.name
+      find('a[data-turbo-method="delete"]').click
+
+      expect(page).to have_content @hospital
+      expect(page).to have_content "Iniciar sesión"
+    end
+  end
+end
