6b88fd4a329ecac2a807f13a84ad74e53d9e6b53
Juan Vasquez <juan@ombulabs.com>
2024-03-11 16:46:00 -0600

    Add hospital tests

--- spec/requests/hospitals_spec.rb
+++ spec/requests/hospitals_spec.rb
@@ -17,4 +17,24 @@ RSpec.describe "hospitals", type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  describe "PUT /hospitals/1" do
+    context "with valid parameters" do
+      let(:valid_params) { attributes_for(:hospital) }
+
+      it "updates the requested hospital" do
+        put hospital_path(hospital), params: {hospital: valid_params}
+        expect(response).to redirect_to(edit_hospital_path(hospital))
+      end
+    end
+
+    context "with invalid parameters" do
+      let(:invalid_params) { attributes_for(:hospital, name: "") }
+
+      it "does not update the requested hospital" do
+        put hospital_path(hospital), params: {hospital: invalid_params}
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 end
---
+++ spec/routing/hospitals_routing_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe HospitalsController do
+  describe "routing" do
+    it "routes to #edit" do
+      expect(get: "/hospitals/1/edit").to route_to("hospitals#edit", id: "1")
+    end
+
+    it "routes to #update via PUT" do
+      expect(put: "/hospitals/1").to route_to("hospitals#update", id: "1")
+    end
+
+    it "routes to #update via PATCH" do
+      expect(patch: "/hospitals/1").to route_to("hospitals#update", id: "1")
+    end
+  end
+end
