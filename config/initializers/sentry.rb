1d673a8c78623beb3f905c625dcc2023d3f0f421
Juan Vasquez <juan@ombulabs.com>
2024-03-13 22:27:14 -0600

    Add Sentry release flag

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 Sentry.init do |config|
   config.dsn = ENV.fetch("SENTRY_DSN", "")
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+  config.release = "#{ENV.fetch("HEROKU_RELEASE_VERSION", "")}-#{ENV.fetch("HEROKU_SLUG_DESCRIPTION", "")}"
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
