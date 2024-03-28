d3bf57d8106ea8e3e20a065f96d479d0564aa157
Juan Vásquez <javasgon@gmail.com>
2024-03-28 08:51:06 -0600

    add smtp config (#531)
    * Add letter_opener gem
    
    * Add production smtp config
    
    * Add mailer views and improve es locals

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,6 +45,10 @@ module ApplicationHelper
     "text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
   end
 
+  def link_button
+    "text-blue-600 dark:text-blue-500 hover:underline"
+  end
+
   def sidebar_classes action
     classes = "flex p-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700"
     classes += " bg-gray-200 dark:bg-gray-700" if controller_name.to_sym.eql?(action)
