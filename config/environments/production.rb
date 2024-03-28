0968dd9d2f6c81523baa2900c82a9d291ad4e914
Juan Vasquez <juan@ombulabs.com>
2024-03-28 09:06:45 -0600

    Update credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -90,7 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = {host: Rails.application.credentials.dig(:smpt, :domain)}
+  config.action_mailer.default_url_options = {host: Rails.application.credentials.dig(:smtp, :domain)}
 
   config.action_mailer.delivery_method = :smtp
 
