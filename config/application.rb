ef5024f77a302acc4d32fa13de01bb5c47e8cd95
Juan Vasquez <juan@ombulabs.com>
2024-03-29 22:39:10 -0600

    Run rake app:update

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative "boot"
 
+# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -10,8 +11,7 @@ require "action_mailer/railtie"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,7 +22,18 @@ module Doctors
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
     config.time_zone = "America/Mexico_City"
     config.i18n.default_locale = "es-MX"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
