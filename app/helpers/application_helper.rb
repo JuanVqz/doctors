b4df36fa595c87d8732098475c50192510a74b62
Juan Vasquez <juan@ombulabs.com>
2024-05-31 19:37:43 -0600

    Delete patient with appointments, hospitalizations, etc.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,6 +47,10 @@ module ApplicationHelper
     'text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'
   end
 
+  def delete_button
+    'text-white bg-red-600 border border-gray-300 focus:outline-none hover:bg-red-800 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:text-white dark:border-red-800 dark:hover:bg-red-700 dark:hover:border-red-600 dark:focus:ring-gray-700'
+  end
+
   def link_button
     'text-blue-600 dark:text-blue-500 hover:underline'
   end
