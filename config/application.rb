e46634236232de6f1a65273ecb09dca16d7b0950
Juan Vasquez <juan@ombulabs.com>
2025-02-09 23:20:18 -0600

    Set to_time_preserves_timezone
    DEPRECATION WARNING: `to_time` will always preserve the full timezone rather than offset of the receiver in Rails 8.1. To opt in to the new behavior, set `config.active_support.to_time_preserves_timezone = :zone`. (called from <top (required)> at /home/runner/work/doctors/doctors/config/environment.rb:7)
    DEPRECATION WARNING: `to_time` will always preserve the full timezone rather than offset of the receiver in Rails 8.1. To opt in to the new behavior, set `config.active_support.to_time_preserves_timezone = :zone`. (called from <top (required)> at /home/runner/work/doctors/doctors/config/environment.rb:7)

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module Doctors
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.active_support.to_time_preserves_timezone = :zone
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
